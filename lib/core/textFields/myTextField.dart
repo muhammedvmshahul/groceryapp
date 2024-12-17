3afb9a5ad067027ed79d242e38a3d79d2c431180
shahul vm <muhammedvmshahul@gmail.com>
2024-12-17 12:04:08 +0530

    updated app icons

--- lib/core/textFields/myTextField.dart
+++ lib/core/textFields/myTextField.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:groceryapp/core/constants/colorConstants.dart';
+
+import '../constants/colorConstants.dart';
 
 
 class CustomInputField extends StatelessWidget {
